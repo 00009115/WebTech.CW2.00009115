0f0f6eb220d9b1f28599ef529ffce8bfc3491e45
00009115 <4BIS1.00009115@gmail.com>
2021-03-28 18:52:50 +0500

    Making Blog Cards Dynamic

--- app.js
+++ app.js
@@ -1,12 +1,80 @@
 const express = require("express");
 const app = express();
 
+const blogs = [
+    {
+        id: 0,
+        type: "pet",
+        title: "Hello!",
+        date: "12:28",
+        text: "salfmasvlk lpsdmdflk mapsoflp maspeefpo psepofk opkas[eekf [ppk[pasek [pkp[sef kp["
+    },
+    {
+        id: 1,
+        type: "science",
+        title: "Hello!",
+        date: "12:28",
+        text: "salfmasvlk lpsdmdflk mapsoflp maspeefpo psepofk opkas[eekf [ppk[pasek [pkp[sef kp["
+    },
+    {
+        id: 2,
+
+        type: "christmas",
+        title: "Yehhooo!",
+        date: "05:09",
+        text: "salfmasvlk lpsdmdflk mapsoflp maspeefpo psepofk opkas[eekf [ppk[pasek [pkp[sef kp["
+    },
+    {
+        id: 3,
+
+        type: "sport",
+        title: "Hello!",
+        date: "12:28",
+        text: "salfmasvlk lpsdmdflk mapsoflp maspeefpo psepofk opkas[eekf [ppk[pasek [pkp[sef kp["
+    },
+    {
+        id: 4,
+        type: "quizz",
+        title: "Yehhooo!",
+        date: "05:09",
+        text: "salfmasvlk lpsdmdflk mapsoflp maspeefpo psepofk opkas[eekf [ppk[pasek [pkp[sef kp["
+    },
+    {
+        id: 5,
+        type: "homework",
+        title: "Hello!",
+        date: "12:28",
+        text: "salfmasvlk lpsdmdflk mapsoflp maspeefpo psepofk opkas[eekf [ppk[pasek [pkp[sef kp["
+    },
+    {
+        id: 6,
+        type: "family",
+        title: "Yehhooo!",
+        date: "05:09",
+        text: "salfmasvlk lpsdmdflk mapsoflp maspeefpo psepofk opkas[eekf [ppk[pasek [pkp[sef kp["
+    },
+    {
+        id: 7,
+        type: "cooking",
+        title: "Hello!",
+        date: "12:28",
+        text: "salfmasvlk lpsdmdflk mapsoflp maspeefpo psepofk opkas[eekf [ppk[pasek [pkp[sef kp["
+    },
+    {
+        id: 8,
+        type: "brainstorming",
+        title: "Yehhooo!",
+        date: "05:09",
+        text: "salfmasvlk lpsdmdflk mapsoflp maspeefpo psepofk opkas[eekf [ppk[pasek [pkp[sef kp["
+    }
+];
+
 app.set("view engine", "pug");
 
 app.use("/static", express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", { blogs: blogs });
 });
 
 app.get("/about", (req, res) => {
@@ -14,11 +82,19 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/blogs", (req, res) => {
-    res.render("pages/blogs");
+    res.render("pages/blogs", { blogs: blogs });
+});
+
+app.get("/blogs/blog", (req, res) => {
+    res.render("pages/blog");
 });
 
 app.get("/contacts", (req, res) => {
     res.render("pages/contacts");
+});
+
+app.get("/add-blog", (req, res) => {
+    res.render("pages/add-blog");
 });
 
 app.listen(8000, (err) => {
