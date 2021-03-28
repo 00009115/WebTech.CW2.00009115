d514f297bb29abefe7f1776495eb4db8cf665c1b
00009115 <4BIS1.00009115@gmail.com>
2021-03-28 15:55:31 +0500

    Creating Contacts Page

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.get("/about", (req, res) => {
     res.render("pages/about");
 });
 
+app.get("/blogs", (req, res) => {
+    res.render("pages/blogs");
+});
+
+app.get("/contacts", (req, res) => {
+    res.render("pages/contacts");
+});
+
 app.listen(8000, (err) => {
     if (err) console.log(err);
     console.log("App is running on port 8000...");
